60488f7581ebb3e3cc77b593877f8d81e9747f2a
Gaurav Alva <gauravgalva@gmail.com>
2025-08-16 16:16:46 +0530

    Portfolio

---
+++ src/config.ts
@@ -0,0 +1,143 @@
+export const siteConfig = {
+  name: "Gaurav G Alva",
+  title: "Data Engineer | Analyst | Web Dev",
+  description: "Portfolio website of Gaurav G Alva",
+  accentColor: "#fb8500   ",//"#DD6B20",//"#1d4ed8",
+  social: {
+    email: "gauravalva.work@gmail.com",
+    linkedin: "https://linkedin.com/in/gaurav-alva",
+    twitter: "https://x.com/gaurav_a_l_v_a_",
+    github: "https://github.com/gaurav-g-alva",
+    gdev: "https://g.dev/gauravgalva",
+
+  },
+  aboutMe:
+    "I'm **Gaurav G Alva**. A final-year **Data Science** student and creative at heart. I connect the dots between data analysis and visual design.\n\nWhile I'm building my expertise in **machine learning** and **Python**, I also bring a strong eye for aesthetics through my work in videography, photography, and editing.\n\nMy goal is to make data not just informative, but also engaging. I am actively looking for an **internship** to apply my skills in a dynamic tech environment.",
+
+  skills: [
+    "Python","Machine Learning","Data Analysis","SQL / PostgreSQL","Java","C","AWS","GCP","Docker",
+  ],
+
+  certifications: [
+    {
+      name: "OCI AI Foundation Associate Certified",
+      issuer: "Oracle",
+      date: "August 2025",
+
+      image: "/images/oci.png",
+      link: "https://catalog-education.oracle.com/pls/certview/sharebadge?id=E984B9047201D95117D079B717349DA736B75719EC26D9AC644E80B491BC7758"
+    },
+    {
+      name: "Prompt Design in Vertex AI Skill Badge",
+      issuer: "Google Cloud",
+      date: "August 2025",
+
+      image: "/images/gc.png",
+      link: "https://www.credly.com/badges/e1051c39-2d6e-45ae-9c57-247f18295f89/linked_in_profile"
+    },
+    {
+      name: "AWS Educate Introduction to Generative AI",
+      issuer: "Amazon Web Services",
+      date: "July 2025",
+      image: "/images/aws.png",
+
+      link: "https://www.credly.com/badges/ac3fd0cf-b1a9-4e24-9648-adddbf09d6cc/linked_in_profile"
+    },
+    {
+      name: "Deloitte Australia - Data Analytics Job Simulation",
+      issuer: "Deloitte",
+      date: "May 2025",
+      image : "/images/data.png",
+      link: "https://forage-uploads-prod.s3.amazonaws.com/completion-certificates/9PBTqmSxAf6zZTseP/io9DzWKe3PTsiS6GG_9PBTqmSxAf6zZTseP_B3LGqKrX9Swhc3YzL_1748596801601_completion_certificate.pdf"
+    },
+    {
+      name: "Deloitte Australia - Cyber Job Simulation",
+      issuer: "Deloitte",
+      date: "May 2025",
+      image: "/images/cyber.png",
+      link: "https://forage-uploads-prod.s3.amazonaws.com/completion-certificates/9PBTqmSxAf6zZTseP/E9pA6qsdbeyEkp3ti_9PBTqmSxAf6zZTseP_B3LGqKrX9Swhc3YzL_1749202238361_completion_certificate.pdf"
+    },
+    {
+      name: "Data Analytics Essentials",
+      issuer: "Cisco",
+      date: "January 2025",
+      image: "/images/cisco.png",
+      link: "https://www.credly.com/badges/d6146aec-6a55-454e-acd6-00441d9eea30"
+    },
+    {
+      name: "Cloud Computing Fundamentals",
+      issuer: "IBM",
+      date: "December 2024",
+      image: "/images/ibm.png",
+      link: "https://www.credly.com/badges/62a06820-4071-4f2c-be55-73358986b655/public_url"
+    },
+    {
+      name: "Build a Website on Google Cloud - Google Skillboost",
+      date: "December 2024",
+      image: "/images/gc1.png",
+      link: "https://www.credly.com/earner/earned/share/611c7d2e-7983-4433-8f27-2e4fdcbcd666"
+    },
+  ],
+
+  projects: [
+    {
+       name: "Web-Based Automatic Timetable Scheduler",
+      description:
+        "A genetic algorithm-based system to automatically generate conflict-free timetables for colleges.",
+      link: "https://new-app-proj-gaurav.streamlit.app/",
+      skills: ["Python", "Genetic Algorithms", "Data Structures","Streamlit","PostgreSQL"],
+
+    },
+    {name: "PatrSrijan - Streamlining Certificate Generation",
+      description:
+        "Developed an automatic certificate generator and sender. The web app fetches participant details from a CSV file, generates certificates, and emails them automatically.",
+      skills: ["Python", "Django", "HTML", "CSS", "Google Cloud", "Google APIs"],
+    },
+    {
+      name: "NoteSwap - Buy and Sell Notes Online",
+      description:
+        "A collaborative web platform enabling students to seamlessly share, upload, and access academic notes and resources categorized by subjects, topics, or courses.",
+        //link: "https://fullstackextensions.com/?ref=devportfolio",
+      skills: ["Python", "Django", "Bootstrap", "Google Cloud", "Google APIs"],
+    },
+  ],
+  experience: [
+    {
+       company: "IEEE VCET Student Branch",
+      title: "Vice Chair - IEEE ComSoc SB",
+      dateRange: "Jan 2025 – Present",
+      bullets: [
+        "Conducted industry-connect project exhibition under IEEE VCET SB.",
+        "Organized events and skill development programs.",
+      ],
+    },
+  ],
+  education: [
+    {
+      school: "Vivekananda College of Engineering and Technology, Puttur",
+      degree: "B.E in Computer Science (Data Science)",
+      dateRange: "Sept 2022 – Aug 2026",
+      achievements: [
+        "GPA: 8.57/10",
+        "Relevant Coursework: Data Structures, OS, TOC, Computer Networks, DBMS, AI & ML, Big Data Analytics, UI/UX Design, Algorithms",
+      ],
+    },
+    {
+      school: "Indraprastha Vidyalaya PU College, Uppinangady",
+      degree: "Higher Secondary Education",
+      dateRange: "June 2020 – July 2022",
+      achievements: [
+        "Percentage: 82%",
+        "Relevant Coursework: Physics, Chemistry, Mathematics, Computer Science (C++)",
+      ],
+    },
+    {
+      school: "Indraprastha Vidyalaya, Uppinangady",
+      degree: "Secondary Education",
+      dateRange: "June 2019 – May 2020",
+      achievements: [
+        "Percentage: 87.36%",
+      ],
+    },
+  ],
+};
