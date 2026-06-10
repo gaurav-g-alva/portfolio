fa9de460ec8c3b0bfd51af15873a7d2fb0de058d
Gaurav Alva <gauravgalva@gmail.com>
2026-06-11 00:58:41 +0530

    Staged Blog Page -2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://gaurav-g-alva.github.io',
+  site: 'https://gauravgalva.in',
   base: '/',
+  integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()],
   },
